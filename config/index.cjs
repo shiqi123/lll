2f3a3f5a276f9a1db2a4ffd1c285bb7d4c06150c
ZzX1998116 <76679392+ZzX1998116@users.noreply.github.com>
2024-01-10 14:53:07 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -31,7 +31,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oqz6O6Rv0xcOqS3W_G9AFw3WoH1I',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'ltkAi7h_WIFp-opBUOvxQWHjnOdPf8WNjRmkaU4roh4',
+      useTemplateId: 'W4JWR-0uwexO7_SBj9s5NEKEh_IBYZJhOCdKsrdXbCs',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '02-12',
       festivals: [
