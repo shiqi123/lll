5bc176d8c2c6206dc59f0cea32289a3da442e370
ZzX1998116 <76679392+ZzX1998116@users.noreply.github.com>
2024-01-10 15:16:06 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'MD0VoD-PL8BxHhl9Evcpr2jm3JWWKuEPqYjEZWa5XuA',
+  CALLBACK_TEMPLATE_ID: '',
 
   CALLBACK_USERS: [
     {
