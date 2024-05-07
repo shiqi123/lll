2722c2027db6345c7ef1fdcb8880ee6c094743f9
ZzX1998116 <76679392+ZzX1998116@users.noreply.github.com>
2024-05-07 11:22:00 +0800

    更新微信模板

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oqz6O6Rv0xcOqS3W_G9AFw3WoH1I',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'MD0VoD-PL8BxHhl9Evcpr2jm3JWWKuEPqYjEZWa5XuA',
+      useTemplateId: 'VGx2XFNhYHXEw8DhFrghzP4MlKTbfPTUf76z-EY1kaA',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '02-12',
       festivals: [
